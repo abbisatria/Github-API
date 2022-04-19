c6ea7185e087f61f771e8b61533bbe26ebb57ecd
abbisatria <abbisatria48@gmail.com>
2022-04-19 17:36:58 +0700

    slicing UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Container, Input, Row, Col, Card, CardBody, CardTitle, CardText, Button, CardFooter, ButtonGroup } from 'reactstrap';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container className="py-5">
+      <Row className="mb-3">
+        <Col md={4}>
+          <Input placeholder="Search..." />
+        </Col>
+      </Row>
+      <Row>
+        {[...Array(4)].map((val, idx) => {
+          return (
+            <Col md={6} className="mb-3" key={String(idx)}>
+              <Card>
+                <CardBody>
+                  <CardTitle tag="h5" className="mb-3">
+                    Github-API
+                  </CardTitle>
+                  <CardText>
+                    <Row>
+                      <Col md={3}>
+                        Typescript
+                      </Col>
+                      <Col md={9}>
+                        Updated 20 minutes ago
+                      </Col>
+                    </Row>
+                  </CardText>
+                  <Button>
+                    Detail Repository
+                  </Button>
+                </CardBody>
+                <CardFooter>
+                  Created 2021-10-16T20:09:14Z
+                </CardFooter>
+              </Card>
+            </Col>
+          )
+        })}
+      </Row>
+      <div className="text-center">
+        <ButtonGroup>
+          <Button outline>
+            Previous
+          </Button>
+          <Button outline>
+            Next
+          </Button>
+        </ButtonGroup>
+      </div>
+    </Container>
   );
 }
 
