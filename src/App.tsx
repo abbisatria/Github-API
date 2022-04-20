5a297e6b838a2dabe134c6073565733f8c04ee69
abbisatria <abbisatria48@gmail.com>
2022-04-20 07:31:53 +0700

    added function paginate & added loading indicator

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react';
 import { Container, Input, Row, Col, Card, CardBody, CardTitle, Button, CardFooter, ButtonGroup } from 'reactstrap';
 import { getLanguage, getListRepo } from './services/github';
 import moment from 'moment';
+import Loader from './components/loader';
 
 interface DataTypes {
   id: number,
@@ -16,12 +17,15 @@ interface DataTypes {
 function App() {
 
   const [data, setData] = useState([]);
+  const [page, setPage] = useState(1);
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
     getData()
   }, []);
 
   const getData = async () => {
+    setIsLoading(true);
     const response = await getListRepo({ sort: 'pushed' })
     if (!response.error) {
       const result: any = await Promise.all(response.data.map(async (val: DataTypes) => {
@@ -33,6 +37,7 @@ function App() {
       }))
       setData(result)
     }
+    setIsLoading(false);
   }
 
   const updated = (value: DataTypes) => {
@@ -49,59 +54,78 @@ function App() {
     } else if (differenceInDays < 31) {
       return `Updated ${new Date().getDate() - new Date(value.pushed_at).getDate()} days ago`
     } else {
+      if (new Date().getFullYear() === new Date(value.pushed_at).getFullYear()) {
+        return `Updated on ${moment(value.pushed_at).format('MMMM D')}`
+      }
       return `Updated on ${moment(value.pushed_at).format('MMMM D, YYYY')}`
     }
   }
 
+  const handleChangePage = (page: number) => {
+    setIsLoading(true);
+    setTimeout(() => {
+      setPage(page);
+      setIsLoading(false);
+    }, 2000);
+  };
+
+  const indexOfLastPost = page * 6;
+  const indexOfFirstPost = indexOfLastPost - 6;
+  const currentPosts = data.slice(indexOfFirstPost, indexOfLastPost);
+  const totalPages = Math.ceil(data.length / 6);
+
   return (
-    <Container className="py-5">
-      <Row className="mb-3">
-        <Col md={4}>
-          <Input placeholder="Search..." />
-        </Col>
-      </Row>
-      <Row>
-        {data.length > 0 && data.map((val: DataTypes) => {
-          return (
-            <Col md={6} className="mb-3" key={String(val.id)}>
-              <Card>
-                <CardBody>
-                  <CardTitle tag="h5" className="mb-3">
-                    {val.name}
-                  </CardTitle>
-                  <div className="card-text mb-3">
-                    <Row>
-                      <Col md={3}>
-                        {val.language || '-'}
-                      </Col>
-                      <Col md={9}>
-                        {updated(val)}
-                      </Col>
-                    </Row>
-                  </div>
-                  <Button onClick={() => window.open(val.html_url)}>
-                    Detail Repository
-                  </Button>
-                </CardBody>
-                <CardFooter>
-                  Created {moment(val.created_at).format('D MMMM YYYY')}
-                </CardFooter>
-              </Card>
-            </Col>
-          )
-        })}
-      </Row>
-      <div className="text-center">
-        <ButtonGroup>
-          <Button outline>
-            Previous
-          </Button>
-          <Button outline>
-            Next
-          </Button>
-        </ButtonGroup>
-      </div>
-    </Container>
+    <>
+      {isLoading && <Loader />}
+      <Container className="py-5">
+        <Row className="mb-3">
+          <Col md={4}>
+            <Input placeholder="Search..." />
+          </Col>
+        </Row>
+        <Row>
+          {currentPosts.length > 0 && currentPosts.map((val: DataTypes) => {
+            return (
+              <Col md={6} className="mb-3" key={String(val.id)}>
+                <Card>
+                  <CardBody>
+                    <CardTitle tag="h5" className="mb-3">
+                      {val.name}
+                    </CardTitle>
+                    <div className="card-text mb-3">
+                      <Row>
+                        <Col md={3}>
+                          {val.language || '-'}
+                        </Col>
+                        <Col md={9}>
+                          {updated(val)}
+                        </Col>
+                      </Row>
+                    </div>
+                    <Button onClick={() => window.open(val.html_url)}>
+                      Detail Repository
+                    </Button>
+                  </CardBody>
+                  <CardFooter>
+                    Created {moment(val.created_at).format('D MMMM YYYY')}
+                  </CardFooter>
+                </Card>
+              </Col>
+            )
+          })}
+        </Row>
+        <div className="text-center">
+          <ButtonGroup>
+            <Button outline onClick={() => handleChangePage(page - 1)} disabled={page === 1}>
+              Previous
+            </Button>
+            <Button outline onClick={() => handleChangePage(page + 1)} disabled={totalPages === page}>
+              Next
+            </Button>
+          </ButtonGroup>
+        </div>
+      </Container>
+    </>
   );
 }
 
---
+++ src/components/loader/index.tsx
@@ -0,0 +1,17 @@
+import "./loader.scss";
+
+const Loader = () => {
+  return (
+    <>
+      <div className="loading">
+        <div className="dot"></div>
+        <div className="dot"></div>
+        <div className="dot"></div>
+        <div className="dot"></div>
+        <div className="dot"></div>
+      </div>
+    </>
+  );
+};
+
+export default Loader;
